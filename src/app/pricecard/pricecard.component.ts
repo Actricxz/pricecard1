0fc7e58178fd8ff443c9541a8053dd991966d56d
Actricxz <bikashwork99@gmail.com>
2022-05-11 15:10:57 +0530

    first commit

---
+++ src/app/pricecard/pricecard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pricecard',
+  templateUrl: './pricecard.component.html',
+  styleUrls: ['./pricecard.component.css']
+})
+export class PricecardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
